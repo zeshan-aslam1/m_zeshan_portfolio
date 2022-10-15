915069c7f6cbc04041ef1a577599c004653a2c45
zeshan-aslam1 <shanirawn5@gmail.com>
2022-10-15 16:35:22 +0500

    zeshan_portfolio

---
+++ portfolio/src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+// import Testimonials from "./components/Testimonials";
+import "./App.css";
+import Tools from "./components/Tools";
+export default function App() {
+  return (
+    <main className="text-gray-400 bg-gray-900 body-font">
+      <Navbar />
+      <About />
+      <Projects />
+      <Skills />
+      <Tools />
+      {/* <Testimonials /> */}
+      <Contact />
+    </main>
+  );
+}
---
+++ portfolio/src/components/About.js
@@ -0,0 +1,46 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <section id="about">
+      <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
+        <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
+            Hi, I'm Muhammad Zeshan Aslam.
+            <br className="hidden lg:inline-block" />
+            Front End Developer
+          </h1>
+          <p className="mb-8 leading-relaxed">
+            A passionate Front End Developer whose dream is to work on a highly
+            scaled application which is used by millions of users, an
+            application that is developed using the cutting edge technologies. I
+            strongly follow the conventions of a technology that I work on so
+            that my work is neat, efficient, readable & reusable. I am willing
+            to work both remotely and in person as well.
+          </p>
+          <div className="flex justify-center">
+            <a
+              href="#contact"
+              className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg"
+            >
+              Work With Me
+            </a>
+            <a
+              href="#projects"
+              className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg"
+            >
+              See My Past Work
+            </a>
+          </div>
+        </div>
+        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+          <img
+            className="object-cover object-center rounded"
+            alt="hero"
+            src="./coding.svg"
+          />
+        </div>
+      </div>
+    </section>
+  );
+}
